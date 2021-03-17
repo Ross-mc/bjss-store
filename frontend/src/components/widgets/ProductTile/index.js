d529b5202f48cc8d8408dbaeb1bb084a8ec05d9e
Vince <vince.picton@bjss.com>
2021-03-17 15:06:15 +0000

    tile clickability and event propagation stopped for buttons

--- frontend/src/components/widgets/ProductTile/index.js
+++ frontend/src/components/widgets/ProductTile/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import styles from "./productTile.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera, faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import Modal from 'react-modal';
 
 export default ({ product, addToBasket, removeFromBasket }) => {
   return (
-    <div className={styles.tile}>
+    <div
+      className={styles.tile}
+      onClick={() => {
+        console.log(product);
+      }}
+    >
       <div className={styles.tileGrid}>
         <div className={styles.imageContainer}>
           <FontAwesomeIcon icon={faCamera} />
@@ -16,14 +22,23 @@ export default ({ product, addToBasket, removeFromBasket }) => {
           <p className={styles.quantity}>Quantity: {product.quantity}</p>
         )}
         {addToBasket && (
-          <button className={styles.cta} onClick={() => addToBasket(product)}>
+          <button
+            className={styles.cta}
+            onClick={(e) => {
+              e.stopPropagation();
+              addToBasket(product);
+            }}
+          >
             <FontAwesomeIcon icon={faShoppingBasket} /> Add to Basket
           </button>
         )}
         {removeFromBasket && (
           <button
             className={styles.secondaryButton}
-            onClick={() => removeFromBasket(product)}
+            onClick={(e) => {
+              e.stopPropagation();
+              removeFromBasket(product);
+            }}
           >
             Remove from Basket
           </button>
