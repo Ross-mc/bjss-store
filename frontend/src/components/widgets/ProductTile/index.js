71247550247345cfa8ebcdde439fde560422f6eb
Patrick Mackridge <patrick.mackridge@bjss.com>
2021-03-17 15:11:24 +0000

    Showing modal of product tile

--- frontend/src/components/widgets/ProductTile/index.js
+++ frontend/src/components/widgets/ProductTile/index.js
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./productTile.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera, faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import Modal from 'react-modal';
 
 export default ({ product, addToBasket, removeFromBasket }) => {
-  const setActive = (event) => {
-    event.preventDefault()
-    console.log(event.target);
-  }
+
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className={styles.tile}>
-      <div className={styles.tileGrid}>
-        <div className={styles.imageContainer}>
-          <FontAwesomeIcon icon={faCamera} />
-        </div>
-        <a href="/" onClick={setActive}>
+    <>
+      <div className={styles.tile} onClick={() => setModalOpen(true)}>
+        <div className={styles.tileGrid}>
+          <div className={styles.imageContainer}>
+            <FontAwesomeIcon icon={faCamera} />
+          </div>
           <h3 className={styles.shortDescription}>{product.shortDescription}</h3>
-        </a>
-        <p className={styles.price}>£{product.price}</p>
-        {product.quantity && (
-          <p className={styles.quantity}>Quantity: {product.quantity}</p>
-        )}
-        {addToBasket && (
-          <button className={styles.cta} onClick={() => addToBasket(product)}>
-            <FontAwesomeIcon icon={faShoppingBasket} /> Add to Basket
-          </button>
-        )}
-        {removeFromBasket && (
-          <button
-            className={styles.secondaryButton}
-            onClick={() => removeFromBasket(product)}
-          >
-            Remove from Basket
-          </button>
-        )}
+          <p className={styles.price}>£{product.price}</p>
+          {product.quantity && (
+            <p className={styles.quantity}>Quantity: {product.quantity}</p>
+          )}
+          {addToBasket && (
+            <button className={styles.cta} onClick={() => addToBasket(product)}>
+              <FontAwesomeIcon icon={faShoppingBasket} /> Add to Basket
+            </button>
+          )}
+          {removeFromBasket && (
+            <button
+              className={styles.secondaryButton}
+              onClick={() => removeFromBasket(product)}
+            >
+              Remove from Basket
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+      <Modal isOpen={modalOpen} onAfterOpen={() => {console.log('MODAL OPEN')}} onRequestClose={() => {setModalOpen(false)}}>
+      <div className={styles.tileGrid}>
+          <div className={styles.imageContainer}>
+            <FontAwesomeIcon icon={faCamera} />
+          </div>
+          <h3 className={styles.shortDescription}>{product.longDescription}</h3>
+          <p className={styles.price}>£{product.price}</p>
+          {product.quantity && (
+            <p className={styles.quantity}>Quantity: {product.quantity}</p>
+          )}
+          {addToBasket && (
+            <button className={styles.cta} onClick={() => addToBasket(product)}>
+              <FontAwesomeIcon icon={faShoppingBasket} /> Add to Basket
+            </button>
+          )}
+          {removeFromBasket && (
+            <button
+              className={styles.secondaryButton}
+              onClick={() => removeFromBasket(product)}
+            >
+              Remove from Basket
+            </button>
+          )}
+        </div>
+        <button onClick={() => setModalOpen(false)}>Close</button>
+      </Modal>
+    </>
   );
 };
