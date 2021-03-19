48fb9b88053fc23792f9fb61d4d0d9e2d5f4fc40
Dan Fryer <danfryer@LPT5380.bjss.co.uk>
2021-03-19 13:35:57 +0000

    sign in page and functionality finsihed

--- frontend/src/components/containers/Login/index.js
+++ frontend/src/components/containers/Login/index.js
@@ -3,41 +3,85 @@ import { Link } from 'react-router-dom';
 
 import React, { useState } from "react";
 
-// import ProductList from "../../widgets/ProductList";
-
-// const mapStateToProps = (state) => {
-//     const { status, products } = state.products.todaysDeals;
-//     return {
-//       status,
-//       products,
-//     };
-//   };
+  class Login extends React.Component{
+    state = {
+      email: "",
+      password: "",
+      loginFailed: false
+    };
   
-//   const mapDispatchToProps = (dispatch) => ({
-//   });
+    handleChange(event, elemToUpdate){
+      const value = event.target.value;
+      let newState = this.state;
+      var stateObject = function(){
+        newState[elemToUpdate] = value;
+        return stateObject;
+      }
   
-//   const Login = connect(mapStateToProps, mapDispatchToProps)(ProductList);
-
-const Login = () => {
-  // const [success, setSuccess] = useState(false);
-  // const submitOrderHandler = () => {
-  //     setSuccess(true);
-  // }
-
-  return (
-      <div>        
+      this.setState(stateObject); 
+    }
+  
+    submitLoginHandler = () => {
+  
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Cookie", "connect.sid=s%3ADSyMF850iT1JlUofh-VVsDGxTjJCzzmH.y58Nebg1BMYqBju4pnB0TmKMNmhL8aQRL6Ns%2FRjvdjw");
+      const raw = JSON.stringify({
+        "email": this.state.email,
+        "name": this.state.name,
+        "address": this.state.address,
+        "postcode": this.state.postcode,
+        "password": this.state.password
+      });
+  
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+  
+      fetch("http://localhost:4001/api/account/sign-in", requestOptions)
+        .then(response => {
+            if(response.status === 200){
+                return response.text()
+            }else{
+                return "invalid Login Credentials"
+            }
+        })
+        .then(result => {
+            if(result === "invalid Login Credentials"){
+                this.setState({loginFailed: true})
+            }else{
+                window.location.href = '/'
+            }
+        })
+        //.then(window.location.href = '/')
+        .catch(error => console.log('error', error))
+        
+    }
+  
+    render(){
+      return (
+        <div>
           <form>
-              <h3>Login</h3>
-              <label for="email">Email: </label>
-              <input type="email" name="email" id="email" />
-              {/* <p class="validation" id="email-validation"></p> */}
-              <label for="password">Password: </label>
-              <input type="password" name="name" id="password" />
-              {/* <p class="validation" id="name-validation"></p> */}
-              <p>Not registered? Click <Link to="/registration" /*onclick={submitDiscountHandler}*/ >Here</Link></p>
+            <h3>Log In</h3>
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" onChange={(event) => this.handleChange(event, 'email')} value={this.state.email}/>
+            
+            <label for="password">Password: </label>
+            <input type="password" name="name" id="password" onChange={(event) => this.handleChange(event, 'password')} value={this.state.password}/>
+            {/* <p class="validation" id="name-validation"></p> */}
+            <button type="button" onClick={this.submitLoginHandler}>Sign In</button>
+            <div className={this.state.loginFailed ? "alert alert-danger" : "d-none"} role="alert">
+                 Invalid Login!
+            </div>
+    
           </form>
-      </div>
-  )
-}
-
+        </div>
+      )
+    }
+  }
+  
+  
 export default Login;
