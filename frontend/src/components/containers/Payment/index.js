4b60a490b47f37e514b528fb4353b3a539cfc16f
Ross McIntyre <ross.mcintyre@bjss.com>
2021-03-26 14:27:32 +0000

    added third payment

--- frontend/src/components/containers/Payment/index.js
+++ frontend/src/components/containers/Payment/index.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Payment = () => {
     return (
       <div>
-        <h1>Third Party Payment thing</h1>
+        <h1>Third Party Payment Processing</h1>
         <p>Go back to <Link to="/redirect?url=/">home</Link></p>
       </div>
     );
